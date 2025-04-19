63faca954eb10927e3a9d68946cac42b9f5aaf9a
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2025-04-19 18:01:14 +0200

    v2

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly DB_HOST: string;
+  readonly DB_PORT: number;
+  readonly DB_USER: string;
+  readonly DB_PASSWORD: string;
+  readonly DB_DATABASE: string;
+}
---
+++ src/models/Website.ts
@@ -0,0 +1,13 @@
+export default class Website {
+
+  url: string;
+  name: string;
+  description: string;
+
+  constructor(url: string, name: string, description: string) {
+    this.url = url;
+    this.name = name;
+    this.description = description;
+  }
+
+}
